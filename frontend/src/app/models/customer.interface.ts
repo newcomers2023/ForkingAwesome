a796ea28da9c27ed2ce8fba0eeeb5a0550141580
Daniel Becker <danielbecker0206@gmail.com>
2023-05-09 10:43:42 +0200

    customer interface

---
+++ frontend/src/app/models/customer.interface.ts
@@ -0,0 +1,10 @@
+export interface Customer {
+    firstName: string;
+    lastName: string;
+    zipCode: number;
+    street: string;
+    houseNumber: string;
+    hasChildren: boolean;
+    //maritalStatus: instant
+    //gender: enum
+  }
