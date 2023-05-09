f1bba5c8001605093de2e9f2e5f82a4ede2b4838
Daniel Becker <danielbecker0206@gmail.com>
2023-05-09 11:27:06 +0200

    added date

--- frontend/src/app/models/customer.interface.ts
+++ frontend/src/app/models/customer.interface.ts
@@ -2,8 +2,8 @@ import { Gender } from "../enums/gender.enum";
 import { martialStatus } from "../enums/martialStatus.enum";
 
 export interface Customer {
-    firstName: string;
-    lastName: string;
+    name: string;
+    birthdate: Date;
     zipCode: number;
     street: string;
     houseNumber: string;
